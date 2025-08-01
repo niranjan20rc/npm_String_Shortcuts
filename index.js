80815e7328c764b9db565360f59df7d7fda7a90f
niranjan20rc <niranjanrc20@gmail.com>
2025-08-01 09:31:44 +0530

    updated on index.js

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-const isVowel = (ch) => {
+export function isVowel(ch){
     return "AEIOUaeiou".indexOf(ch) !== -1;
 };
 
-const isConsonant = (ch) => {
+export  function isConsonant(ch){
     return /^[a-zA-Z]$/.test(ch) && !isVowel(ch);
 };
 
-const isPalindrome=(str1,str2)=>{
+export function isPalindrome(str1,str2){
    const rev=str1.split("").reverse().join("");
    return rev===str2
 }
 
-const isAnagram=(str1,str2)=>{
+export function isAnagram(str1,str2){
    const sorted1=str1.split("").sort().join("");
    const sorted2=str2.split("").sort().join("");
    return sorted1===sorted2;
 }
 
-const isPangram=(str)=>{
+export function isPangram(str){
     let set = new Set(str.split(""));
     return set.size===26;
 }
-export default  {
-    isVowel,
-    isConsonant,
-    isPalindrome,
-    isAnagram,
-    isPangram,
-};
